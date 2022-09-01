879072666224d1e93f63204d3916e0e38c00b25b
microncubed <andrew@microncubed.com>
2022-09-01 10:38:25 -0500

    sorting out directory structure

---
+++ src/models.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 11 11:19:49 2019
+
+@author: andrew.ferguson
+"""
+import numpy as np
+import csv
+import os
+
+path = os.getcwd()
+basepath = os.path.dirname(path)
+
+##Builds up the dictionaries of thermal parameters
+
+k = {}
+c = {}
+rho = {}
+
+filename = basepath+"/src/thermal_properties.csv"
+
+with open(filename,'r') as f:
+    csvreader = csv.DictReader(f)
+    for row in csvreader:
+        k[row['material']]= float(row['thermal_conductivity'])
+        c[row['material']]= float(row['specific_heat'])
+        rho[row['material']]= float(row['density'])
+
+##
+          
+def test2d():    
+    nx=101
+    ny=101
+    dx = 3e-6
+    dy = 1e-6
+
+    
+    x_lim = (nx-1)*dx/2
+    y_lim = (ny-1)*dy/2
+    
+    x = np.linspace(-x_lim,x_lim,nx)
+    y = np.linspace(-y_lim,y_lim,ny)
+    
+    k_in = np.zeros((nx,ny))
+    k_in[:,:] =k['water']
+    
+    Q_in = np.zeros((nx,ny))
+    Q_in_val = 1
+    Q_in[:,:] = Q_in_val
+    
+    v_in = np.zeros((nx,ny))
+    c_in = np.zeros((nx,ny))
+    rho_in = np.zeros((nx,ny))
+    
+    c_in[:,:]=c['water']
+    rho_in[:,:]=rho['water']
+    
+    return [k_in,c_in,rho_in,Q_in,v_in,x,y,dx,dy,nx,ny]
+
+def single_wire_membrane(v = 0e-3,wet=True):
+    nx=401
+    ny=151
+    dx = 2.5e-6
+    dy = 1e-6
+    
+    x_lim = (nx-1)*dx/2
+    y_lim = (ny-1)*dy/2
+    
+    x = np.linspace(-x_lim,x_lim,nx)
+    y = np.linspace(-y_lim,y_lim,ny)
+    
+    k_in = np.zeros((nx,ny))
+    c_in = np.zeros((nx,ny))
+    rho_in = np.zeros((nx,ny))
+    Q_in = np.zeros((nx,ny))       
+    
+    v_max=-v
+    
+    if wet == True:
+        k_in[:,50:101 ] = k['water']
+        c_in[:,50:101] = c['water']
+        rho_in[:,50:101] = rho['water']
+    elif wet == False:
+        k_in[:,50:101 ] = k['air']
+        c_in[:,50:101] = c['air']
+        rho_in[:,50:101] = rho['air']
+    
+    ##put some glass on top
+    k_in[:,101:] = k['silicon_dioxide']
+    c_in[:,101:] = c['silicon_dioxide']
+    rho_in[:,101:] = rho['silicon_dioxide']
+    
+    
+    ##silicon dioxide underneath
+    k_in[:,48:50] = k['silicon_dioxide']
+    c_in[:,48:50] = c['silicon_dioxide']
+    rho_in[:,48:50] = rho['silicon_dioxide']
+   
+     
+    ##silicon dioxide underneath
+    k_in[:,0:48] = k['silicon_dioxide']
+    c_in[:,0:48] = c['silicon_dioxide']
+    rho_in[:,0:48] = rho['silicon_dioxide']
+
+    ##and some air
+    k_in[200-50:200+50,0:48] = k['air']
+    c_in[200-50:200+50,0:48] = c['air']
+    rho_in[200-50:200+50,0:48] = rho['air']
+
+    ##and some silicon
+    k_in[0:200-50,0:48] = k['silicon']
+    c_in[0:200-50,0:48] = c['silicon']
+    rho_in[0:200-50,0:48] = rho['silicon']
+
+    ##and some silicon
+    k_in[200+50:,0:48] = k['silicon']
+    c_in[200+50:,0:48] = c['silicon']
+    rho_in[200+50,0:48] = rho['silicon']
+        
+    heats = 8
+    
+    Q_in[(200-5):(200+6),49:50] = heats
+      
+  
+    v_in = np.zeros((nx,ny))
+    for yval in range(50,101):
+        v_in[:,yval] = v_max - 4*v_max/50**2*(yval-75)**2
+        
+    return [k_in,c_in,rho_in,Q_in,v_in,x,y,dx,dy,nx,ny]
+
---
+++ src/pdeSim.py
@@ -0,0 +1,100 @@
+import numpy as np
+from scipy.sparse.linalg import spsolve
+from scipy.sparse import coo_matrix
+
+class PoissonFlow2D:
+    """
+    Contains a sparse solve method for the steady state of the advection-diffusion equation.
+
+    """	
+
+    def __init__(self, Q, kappa, v, dx,dy,c,rho,freq):        
+        self.Q=Q
+        self.kappa=kappa
+        self.v=v
+        self.dx=dx
+        self.dy=dy
+        self.c=c
+        self.rho=rho        
+        self.freq=freq
+        
+    def sparseSolve(self):
+            row = []
+            col = []
+            data = []
+            nx = self.kappa.shape[0]
+            ny = self.kappa.shape[1]
+          
+
+            for j in range(ny):
+                for i in range(nx):
+                    Adiag = j*nx+i
+                    if j==0:
+                        self.Q[i,j] = 0
+                        row.append(Adiag)
+                        col.append(Adiag)
+                        data.append(1)
+                    elif j ==(ny-1):
+                        self.Q[i,j] = 0
+                        row.append(Adiag)
+                        col.append(Adiag)
+                        data.append(1)
+                    elif i ==0:
+                        self.Q[i,j] = 0
+                        row.append(Adiag)
+                        col.append(Adiag)
+                        data.append(1)
+                    elif i==(nx-1):
+                        self.Q[i,j] = 0
+                        row.append(Adiag)
+                        col.append(Adiag)
+                        data.append(1)
+                    else:
+                        data.append(-1/2*(self.kappa[i-1,j]+self.kappa[i-1,j-1])*self.dy/self.dx-\
+                        1/2*self.c[i,j]*self.rho[i,j]*self.v[i,j]*self.dy-\
+                        1/2*(self.kappa[i,j]+self.kappa[i,j-1])*self.dy/self.dx+\
+                        1/2*self.c[i,j]*self.rho[i,j]*self.v[i,j]*self.dy-\
+                        1/2*(self.kappa[i-1,j]+self.kappa[i,j])*self.dx/self.dy-\
+                        1/2*(self.kappa[i,j-1]+self.kappa[i-1,j-1])*self.dx/self.dy-\
+                        self.c[i,j]*self.rho[i,j]*self.freq*np.pi*2*self.dx*self.dy*1j)
+                        
+                        row.append(Adiag)
+                        col.append(Adiag)
+                        
+                        data.append(1/2*(self.kappa[i-1,j]+self.kappa[i-1,j-1])*self.dy/self.dx-\
+                        1/2*self.c[i,j]*self.rho[i,j]*self.v[i,j]*self.dy)
+                        row.append(Adiag)
+                        col.append(Adiag-1)
+                        
+                        data.append(1/2*(self.kappa[i-1,j]+self.kappa[i,j])*self.dx/self.dy)
+                        row.append(Adiag)
+                        col.append(Adiag+nx)
+                        
+                        data.append(1/2*(self.kappa[i,j]+self.kappa[i,j-1])*self.dy/self.dx+\
+                        1/2*self.c[i,j]*self.rho[i,j]*self.v[i,j]*self.dy)
+                        row.append(Adiag)
+                        col.append(Adiag+1)
+                        
+                        data.append(1/2*(self.kappa[i,j-1]+self.kappa[i-1,j-1])*self.dx/self.dy)
+                        row.append(Adiag)
+                        col.append(Adiag-nx)
+                                          
+            As=coo_matrix((data, (row, col)), shape=(nx*ny, nx*ny))
+            As = As.tocsr()
+                          
+            Q_in_flat = np.zeros(nx*ny)
+            for j in range(ny):
+                for i in range(nx):
+                    Q_in_flat[nx*j+i] = self.Q[i,j]
+            
+
+            n = np.zeros(nx*ny,dtype=complex)
+            n = spsolve(As,-Q_in_flat)
+            
+            unwrapped = np.zeros((nx,ny),dtype=complex)
+            for j in range(ny):
+                for i in range(nx):
+                    unwrapped[i,j]= n[nx*j+i]
+        
+            return unwrapped
+            
